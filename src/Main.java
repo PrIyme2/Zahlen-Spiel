461838e259310541881a6980b8c8a30c0e0093c4
PrIyme2 <marafa@student.tgm.ac.at>
2026-09-19 17:29:23 +0200

    erstellung von .gitignore und temp main klasse

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Startklasse für das Zahlen-Gewinnspiel.
+ * @author Arafa Fateh
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Zahlen-Gewinnspiel");
+    }
+}
